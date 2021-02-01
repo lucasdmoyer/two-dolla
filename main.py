69497548706f805b43da7c7abc6bf50ff03f4961
Lucas Moyer <lucasdmoyer@gmail.com>
2021-01-31 22:20:07 -0600

    stock data api

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 import json
 from yahoofinancials import YahooFinancials as YF
 import time
@@ -34,19 +36,16 @@ async def marketCap(ticker):
     return getMarketCap(ticker)
 
 @app.get("/getStocks")
-async def getStocks():
-    # data = getData("AAPL")
-    # print(data["High"])
-    # return(data["High"])
-    tickers = getTickers()
-    #tickers = ["MSFT","AAPL"]
-    data = getStocksData(tickers, 365)
-    print(data)
-    return("Success! {} stock data gotten".format(len(data)))
-    # if (os.path.exists("../stocks.pkl")):
-    #     with open("../stocks.pkl", "rb") as pkl_handle:
-    #         stocks = pickle.load(pkl_handle)
-    #         return(stocks["AAPL"])d
+async def getStocks(ticker_data: str="MSFT,TSLA,AAPL,T",days_back: int = 365):
+    tickers = ticker_data.split(",")
+    stocks = getStockData(tickers,365)
+    result={}
+    for stock in list(stocks):
+        stocks[stock]['Date'] = stocks[stock].index.strftime('%Y-%m-%d')
+        result[stock] = stocks[stock].to_dict()
+    json_compatible_item_data = jsonable_encoder(result)
+    return json_compatible_item_data
+
 
 @app.post("/portfolio")
 async def portfolio(file_path,file: UploadFile=File(...)):
@@ -161,65 +160,177 @@ class BsmModel:
             print("option type can only be c or p")
 
 
+def convert_time(epoch):
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch))
 
-# function to help process csv inputs
+def getStockData(tickers, days_back):
+    stocks = {}
+    epoch_time = int(time.time())
+    day_epoch = 60*60*24
+    for tick in tqdm(tickers):
+        try:
+            stock_data = data.DataReader(tick, 
+                        start=convert_time(epoch_time - (days_back* day_epoch)), 
+                        end=convert_time(epoch_time), 
+                        data_source='yahoo')
+            stocks[tick] = stock_data 
+        except:
+            print("Skipping stock for {}, bad data :<".format(tick))
+    return stocks
+
+def getSP500Tickers():
+    table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    df = table[0]
+    df.to_csv('S&P500-Info.csv')
+    df.to_csv("S&P500-Symbols.csv", columns=['Symbol'])
+    tickers = df['Symbol']
+    return tickers
+
+
+def getPortReturns(stocks):
+    df = pd.DataFrame()
+    #df.index = stocks[list(stocks.keys())[0]].index
+    for stock in list(stocks):
+        df[stock] = stocks[stock]['simple_returns']
+    return df.dropna()
+
+
+def addMACD(stocks):
+    for stock in list(stocks):
+        exp1 = stocks[stock]['Adj Close'].ewm(span=12, adjust=False).mean()
+        exp2 = stocks[stock]['Adj Close'].ewm(span=26, adjust=False).mean()
+        stocks[stock]['macd'] = exp1-exp2
+        stocks[stock]['macd_signal'] = stocks[stock]['macd'].ewm(
+            span=9, adjust=False).mean()
+    return(stocks)
+
+
+def computeRSI(data, time_window):
+    diff = data.diff(1).dropna()        # diff in one field(one day)
+
+    # this preservers dimensions off diff values
+    up_chg = 0 * diff
+    down_chg = 0 * diff
+
+    # up change is equal to the positive difference, otherwise equal to zero
+    up_chg[diff > 0] = diff[diff > 0]
+
+    # down change is equal to negative deifference, otherwise equal to zero
+    down_chg[diff < 0] = diff[diff < 0]
+    up_chg_avg = up_chg.ewm(com=time_window-1, min_periods=time_window).mean()
+    down_chg_avg = down_chg.ewm(
+        com=time_window-1, min_periods=time_window).mean()
+
+    rs = abs(up_chg_avg/down_chg_avg)
+    rsi = 100 - 100/(1+rs)
+    return rsi
+
+
+def addRSI(stocks, time_window):
+    for stock in list(stocks):
+        stocks[stock]["RSI"] = computeRSI(
+            stocks[stock]["Adj Close"], time_window)
+    return(stocks)
+
+
+def addBB(stocks, time_window):
+    for stock in list(stocks):
+        stocks[stock]['MA20'] = stocks[stock]['Adj Close'].rolling(
+            window=time_window).mean()
+        stocks[stock]['20dSTD'] = stocks[stock]['Adj Close'].rolling(
+            window=time_window).std()
+        stocks[stock]['UpperBB'] = stocks[stock]['MA20'] + \
+            (stocks[stock]['20dSTD'] * 2)
+        stocks[stock]['LowerBB'] = stocks[stock]['MA20'] - \
+            (stocks[stock]['20dSTD'] * 2)
+        stocks[stock]['LowerBB_dist'] = stocks[stock]['LowerBB'] - \
+            stocks[stock]['MA20']
+        stocks[stock]['UpperBB_dist'] = stocks[stock]['MA20'] - \
+            stocks[stock]['UpperBB']
+    return(stocks)
+
+
+def addReturns(stocks):
+    for stock in list(stocks):
+        stocks[stock]['simple_returns'] = stocks[stock]['Adj Close'].pct_change()
+        stocks[stock]['log_returns'] = np.log(
+            stocks[stock]['simple_returns']+1)
+        stocks[stock]['cum_daily_return'] = (
+            (1 + stocks[stock]['simple_returns']).cumprod() - 1)
+    return(stocks)
+
+
+def addVol(stocks, periods):
+    for stock in list(stocks):
+        stocks[stock]['volatility'] = (stocks[stock]['simple_returns'].rolling(
+            periods).std() * np.sqrt(periods))
+    return stocks
+
+
+def lagFeatures(stocks, features, periods, returns):
+    # sets the columns we want in our final df
+    cols_wanted = features + returns
+    stocks_lagged = stocks.copy()
+    print(f'The columns wanted are {cols_wanted}')
+    for stock in list(stocks):
+        stocks_lagged[stock][features] = stocks_lagged[stock][features].shift(
+            periods)
+        stocks_lagged[stock] = pd.DataFrame(
+            stocks_lagged[stock], columns=cols_wanted)
+    return(stocks_lagged)
+
+
+def getRandomWeights(numstocks):
+    weights = np.random.rand(numstocks)
+    return (weights/np.sum(weights))
+
+
+def getPortWeightedReturns(port_ret, weights):
+    assert(len(port_ret.columns) == len(weights))
+    return port_ret.iloc[:, 0:len(weights)].mul(weights, axis=1).sum(axis=1)
+# getRandomWeights(50)
+
+
+def getPortWeightedVol(port_ret, weights):
+    cov_mat = port_ret.cov()
+    #cov_mat_annual = cov_mat * 252
+    # cov_mat_annual
+    port_vol = np.sqrt(np.dot(weights.T, np.dot(cov_mat, weights)))
+    return port_vol
+
+
+def getPortWeightedAnnualReturn(port_ret, weights):
+    returns = getPortWeightedReturns(port_ret, weights)
+
+    mean_return_daily = np.mean(returns)
+    # Calculate the implied annualized average return
+    mean_return_annualized = ((1+mean_return_daily)**252)-1
+    return(mean_return_annualized)
+
+def dict_2_panel(stocks):  
+    df = pd.DataFrame()
+    for stock in list(stocks):
+        stocks[stock]['ticker'] = stock
+        df = df.append(stocks[stock])
+    return df.dropna()
+
+def graphPrice(stocks, ticker):
+    plt.figure(figsize=(10,10))
+    plt.plot(stocks[ticker].index, stocks[ticker]['Adj Close'])
+    plt.xlabel("Date")
+    plt.ylabel("Price ($)")
+    plt.title(ticker + " price")
+    plt.savefig('./graphs/{}_price.png'.format(ticker))
+    
+def getSP500Tickers():
+    table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    df = table[0]
+    df.to_csv('S&P500-Info.csv')
+    df.to_csv("S&P500-Symbols.csv", columns=['Symbol'])
+    tickers = df['Symbol']
+    return list(tickers)
+
 def convertBytesToString(bytes):
     data = bytes.decode('utf-8').splitlines()
     df = pd.DataFrame(data)
     return(df)
-
-def getMarketCap(ticker):
-    req = YF(ticker).get_summary_data()[ticker]['marketCap']
-    return(req)
-
-def convert_time(epoch):
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch))
-
-def getTickers():
-    table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    df = table[0]
-    #df.to_csv('S&P500-Info.csv')
-    #df.to_csv("S&P500-Symbols.csv", columns=['Symbol'])
-    tickers = df['Symbol'][:50]
-    return tickers
-
-def getStocksData(tickers, days_back):
-    epoch_time = int(time.time())
-    day_epoch = 60*60*24
-    stocks = {}
-    print(tickers)
-    for tick in tqdm(tickers):
-        #stock_data = getData(tick)
-        #stocks[tick] = stock_data 
-        # stock_data = data.DataReader(tick, 
-        #                 start=convert_time(epoch_time - (10* day_epoch)), 
-        #                 end=convert_time(epoch_time), 
-        #                 data_source='yahoo')
-        # return(stock_data)
-        try:
-            stock_data = data.DataReader(tick, 
-                        start=convert_time(epoch_time - (int(days_back)* day_epoch)), 
-                        end=convert_time(epoch_time), 
-                        data_source='yahoo')
-            stocks[tick] = stock_data 
-        except Exception as e:
-            print("ERROR : "+str(e))
-            print("Skipping stock for {}, bad data :<".format(tick))
-    return stocks
-
-# def getData(tick):
-#     epoch_time = int(time.time())
-#     day_epoch = 60*60*24
-#     try:
-#         print("trying!!")
-#         stock_data = data.DataReader(tick, 
-#                     start=convert_time(epoch_time - (10* day_epoch)), 
-#                     end=convert_time(epoch_time), 
-#                     data_source='yahoo')
-#         print("again!!")
-#         return(stock_data)
-
-#     except Exception as e:
-#         print("ERROR : "+str(e))
-#         print("Skipping stock for {}, bad data :<".format(tick))
-    
